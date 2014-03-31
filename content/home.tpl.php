9d140f1f0a184a2742003aeb6dc9ca4ba1dc8d1e
Thomas Semmler <ts@ip.co.at>
2014-03-31 17:04:08 +0200

    Bugfixes; Added ionicons... maybe my new to-go icon font?

--- content/home.tpl.php
+++ content/home.tpl.php
@@ -1,9 +1,9 @@
 <div class="wrapper_global">
-	<p class="fill--color-primary swatch swatch--listed txt--color-dark tacenter">Color Primary</p>
-	<p class="fill--color-secondary swatch swatch--listed txt--color-dark tacenter">Color Secondary</p>
-	<p class="fill--color-trinary swatch swatch--listed grid--last txt--color-white tacenter">Color Trinary</p>
-	<p class="fill--color-dark swatch swatch--listed txt--color-bright tacenter">Color Dark</p>
-	<p class="fill--color-bright swatch swatch--listed txt--color-dark tacenter">Color Bright</p>
-	<p class="fill--color-bg swatch swatch--listed grid--last txt--color-dark tacenter">Color Bg</p>
+	<p class="fill--color-primary swatch swatch--listed txt--color-dark tacenter">Color Primary<span class="icon"><i class="ion-ios7-cloudy-night-outline"></i></span></p>
+	<p class="fill--color-secondary swatch swatch--listed txt--color-dark tacenter">Color Secondary<span class="icon"><i class="ion-beaker"></i></span></p>
+	<p class="fill--color-trinary swatch swatch--listed grid--last txt--color-white tacenter">Color Trinary<span class="icon"><i class="ion-coffee"></i></span></p>
+	<p class="fill--color-dark swatch swatch--listed txt--color-bright tacenter">Color Dark<span class="icon"><i class="ion-grid"></i></span></p>
+	<p class="fill--color-bright swatch swatch--listed txt--color-dark tacenter">Color Bright<span class="icon"><i class="ion-ios7-arrow-right"></i></span></p>
+	<p class="fill--color-bg swatch swatch--listed grid--last txt--color-dark tacenter">Color Bg<span class="icon"><i class="ion-ios7-arrow-thin-right"></i></span></p>
 	<div class="wrapper_global--footer"></div>
 </div>
